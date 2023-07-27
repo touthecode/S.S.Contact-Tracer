71ac0fed2f471ab06c3f5c090423beec6ae1491d
touthecode <130022845+touthecode@users.noreply.github.com>
2023-07-28 05:48:00 +0800

    fixed errors + added temp run for checking

--- Program Design-GUI.py
+++ Program Design-GUI.py
@@ -1,10 +1,25 @@
 # Import the tools required for the program
-import tkinter as tk
+import tkinter
 import csv
 from tkinter import ttk
 
 # Make a class that will make the GUI accessible to the main program 
+class CTProgramGUI:
+
 # Make the base app graphics and dimensions
+    def __init__(gui):
+        gui.window = tkinter.Tk()
+        gui.window.title("Covid 19 Contact Tracing App")
+
+        frame = tkinter.Frame(gui.window)
+        frame.pack()
+        gui.window.mainloop()
+
+
 # Make inputs into retrievable string variables
 # Create and design widgets to input data
 # Make a function to run the whole thing
+
+
+app = CTProgramGUI()
+app.run
