76a3dfe22856f8b2f7b74a19fc4c64999ee8882d
touthecode <130022845+touthecode@users.noreply.github.com>
2023-07-27 21:04:20 +0800

    added pseudocodes

---
+++ Main Program.py
@@ -0,0 +1,6 @@
+# Import the tools required for the program
+# Create a base class to inherit the GUI class
+# Create a button to store inputted data entries
+# Create a file to store inputted data entries
+# Create a button to search inputted data entries
+# Run the file
---
+++ Program Design-GUI.py
@@ -0,0 +1,6 @@
+# Import the tools required for the program
+# Make a class that will make the GUI accessible to the main program 
+# Make the base app graphics and dimensions
+# Make inputs into retrievable string variables
+# Create and design widgets to input data
+# Make a function to run the whole thing
