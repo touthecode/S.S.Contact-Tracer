4bb2769a6c7b9bc99b2c2876a7860711c2b97238
touthecode <130022845+touthecode@users.noreply.github.com>
2023-07-28 07:40:04 +0800

    renamed gui name and added tools at main

--- Main Program.py
+++ Main Program.py
@@ -1,4 +1,9 @@
 # Import the tools required for the program
+from Program Design-GUI import *
+import tkinter
+import csv
+from tkinter import ttk
+
 # Create a base class to inherit the GUI class
 # Create a button to store inputted data entries
 # Create a file to store inputted data entries
