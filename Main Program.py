1b0164e6dc5b1e310798c1efbf2efec1b5eb8522
touthecode <130022845+touthecode@users.noreply.github.com>
2023-07-28 22:10:18 +0800

    successfully added submit function and entry saves

--- Main Program.py
+++ Main Program.py
@@ -2,7 +2,7 @@
 from designwidgetgui import *
 import tkinter
 import csv
-from tkinter import ttk
+from tkinter import messagebox, simpledialog
 
 # Create a base class to inherit the GUI class
 class entryandsearch(CTProgramGUI):
@@ -11,11 +11,18 @@ class entryandsearch(CTProgramGUI):
     def __init__(gui):
         super().__init__()
         
-        enter = tkinter.Button(gui.frame, text="Submit data")
+        enter = tkinter.Button(gui.frame, text="Submit data", command=gui.register)
         enter.grid(row=2, column=0, sticky="news", padx=20, pady=10)
-
+        search = tkinter.Button(gui.frame, text="Search data")
+        search.grid(row=3, column=0, sticky="news", padx=20, pady=10)
 
 # Create a file to store inputted data entries
+    def register(gui):
+        new_reg = [gui.name.get(), gui.date.get(), gui.gender.get(), gui.age.get(), gui.number.get(), gui.email.get(), gui.contacts.get()]
+        with open("data_entries.csv", "a", newline='') as file:
+            entry = csv.writer(file)
+            entry.writerow(new_reg)
+            messagebox.showinfo("Complete!", "Your information has been recorded!")
 # Create a button to search inputted data entries
 
 # Run the file 
