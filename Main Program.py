9d1477b12be4db7be413da980388700a9273ba0b
touthecode <130022845+touthecode@users.noreply.github.com>
2023-07-28 10:19:32 +0800

    debugged programs and added enter button

--- Main Program.py
+++ Main Program.py
@@ -1,11 +1,23 @@
 # Import the tools required for the program
-from Program Design-GUI import *
+from designwidgetgui import *
 import tkinter
 import csv
 from tkinter import ttk
 
 # Create a base class to inherit the GUI class
-# Create a button to store inputted data entries
+class entryandsearch(CTProgramGUI):
+
+# Create buttons to store inputted data entries and access search entries
+    def __init__(gui):
+        super().__init__()
+        
+        enter = tkinter.Button(gui.frame, text="Submit data")
+        enter.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+
+
 # Create a file to store inputted data entries
 # Create a button to search inputted data entries
-# Run the file
+
+# Run the file 
+app = entryandsearch()
+app.run()
--- designwidgetgui.py
+++ designwidgetgui.py
@@ -1,22 +1,23 @@
-# Import the tools required for the program
 import tkinter
 from tkinter import ttk
 
 # Make a class that will make the GUI accessible to the main program 
 class CTProgramGUI:
-
-# Make the base app graphics and dimensions
+    # Make the base app graphics and dimensions
     def __init__(gui):
         gui.window = tkinter.Tk()
         gui.window.title("Covid 19 Contact Tracing App")
 
-        frame = tkinter.Frame(gui.window)
-        frame.pack()
+        gui.frame = tkinter.Frame(gui.window)
+        gui.frame.pack()
 
-        user_input_frame =tkinter.LabelFrame(frame, text="General User Information")
-        user_input_frame.grid(row= 0, column= 0, padx= 20, pady =20)
+        # Make inputs into retrievable string variables
+
+        # Create and design widgets to input data
+        user_input_frame = tkinter.LabelFrame(gui.frame, text="General User Information")
+        user_input_frame.grid(row=0, column=0, padx=20, pady=20)
         
-
+        # Rest of your widget creation code...
         full_name_label = tkinter.Label(user_input_frame, text="Full Name: ")
         full_name_label.grid(row= 0, column= 0)
         date_label = tkinter.Label(user_input_frame, text="Date: ")
@@ -47,20 +48,14 @@ class CTProgramGUI:
         email_info_input = tkinter.Entry(user_input_frame)
         email_info_input.grid(row= 3, column= 2)
 
+
         for widget in user_input_frame.winfo_children():
             widget.grid_configure(padx=20, pady=10)
 
-
-        gui.window.mainloop()
-
-
-
+    # Make a function to run the whole thing
+    def run(gui):
+        gui.frame.mainloop()
 
 
-# Make inputs into retrievable string variables
-# Create and design widgets to input data
-# Make a function to run the whole thing
 
 
-app = CTProgramGUI()
-app.run
